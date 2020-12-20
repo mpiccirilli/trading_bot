f474aa666f7d2fead5f8a8087f8e94b506db6750
Michael Piccirilli <michael.r.piccirilli@gmail.com>
2020-12-19 16:18:25 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+TD_CLIENT_ID=''
+TD_REFRESH_TOKEN=''
+TD_GRANT_TYPE=''
+TD_CASH_ACCOUNT=''
+TD_MARGIN_ACCOUNT=''
+GBQ_ORDERS_TABLE=''
+GBQ_CREDS_TABLE=''
+SG_KEY=''
+BQ_CREDS={
+  "type": "",
+  "project_id": "",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "",
+  "client_id": "",
+  "auth_uri": "",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "",
+  "client_x509_cert_url": ""
+}
